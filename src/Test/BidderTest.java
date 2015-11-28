a69b4db17e32d85ebe4ebfe61a2059c658f83255
CaptainDewberry <cds34@uw.edu>
2015-11-28 06:07:17 -0800

    Update BidderTest.java

--- src/Test/BidderTest.java
+++ src/Test/BidderTest.java
@@ -1,4 +1,9 @@
-package test;
+* To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
 
 import static org.junit.Assert.*;
 
@@ -8,7 +13,8 @@ import org.junit.Test;
 import model.AuctionCalendar;
 import model.Auction;
 import model.Bid;
-import model.Bidder;
+//import model.Bidder; //Bidder class is in the users package
+import users.Bidder;
 import model.Item;
 import users.NonProfitOrganization;
 import users.ACEmployee;
@@ -19,39 +25,74 @@ import java.util.List;
 import java.util.HashMap;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 
 
 public class BidderTest {
 	//Bidder user for test instance
 	private Bidder testBidder;
-	private Item testItem;
-	private Credit testCredit;
+	private Item testItem1;
+        private Item testItem2;
+	//private Credit testCredit; //dont need this anymore
 	private Auction testAuction;
+        private NonProfitOrganization npf;
 	
 	//Setting up the test Bidder instance
 	@Before
 	public void setUp() throws Exception {
 		testBidder = new Bidder("Chris");//AuctionCentral.IBIDDER
-		testItem = new Item("Pokemon Cards", 5);
-		testCredit = new Credit(testItem, 250.0, testBidder.getUsername());
-		testAuction = new Auction("Non-Profit Orangization", new ArrayList<Item>(), new AuctionCalendar(), "03:00");
+		//testItem = new Item("Pokemon Cards", 5); //this is wrong. Item's constructor takes 3 arguments- name, quantity, and min start bid
+                //add this
+                testItem1 = new Item("Pokemon Cards", 5, 100.0); //5 Pokemon Cards starting at $100 bid
+                //testCredit = new Credit(testItem, 250.0, testBidder.getUsername()); //get rid of
+		//testAuction = new Auction("Non-Profit Orangization", new ArrayList<Item>(), new AuctionCalendar(), "03:00"); //Auction constructor takes 4 arguments- NonProfitOrganization theOwner, LocalDateTime theStartDate, LocalDateTime theEndDate, List<Item> theItems
+                //In order to test Auction, you must create a NonProfitOrganization instance, 2 LocalDateTime instances (start and end), and a list of Items
+                npf = new NonProfitOrganization("HSPA", "Humane Society of Pennsylvania"); 
+                testAuction = npf.createAuction();
+                testAuction.addItem(testItem1);
+                testAuction.setMyStartDate(LocalDateTime.now());
+                testAuction.setMyEndDate(testAuction.getStartDate().plusMonths(2));
+                //OR
+                /**
+                LocalDateTime start = LocalDateTime.now(); //auction begins right now
+                LocalDateTime end = start.plusMonths(3); //and ends in 3 days
+                //create list of Items
+                List<Item> items = new ArrayList<Item>();
+                items.add(testItem1);
+                testAuction = new Auction(npf, start, end, items);  
+                * **/
 	}
 	//Testing the Constructor of Bidder Class
 	@Test
 	public void testBidder() {
 		Bidder bidder1 = new Bidder("Chris");
-		assertEquals(testBidder, bidder1);
+		assertEquals(testBidder.getUsername(), bidder1.getUsername());
 	}
-    @Test
+        @Test
 	public void testchooseItem() {
-    	testBidder.addBid(testAuction, testCredit);
-		assertTrue(testBidder.viewBids().contains(testCredit));
+                //create new AuctionCalendar instance, adding the testAuction which contains testItem1
+                AuctionCalendar ac = new AuctionCalendar();
+                ac.addAuction(testAuction);
+                ac.addUser(npf);
+                testBidder.chooseAuction(ac); //testBidder will choose the Auction defined above
+                
+                Item item = testBidder.chooseItem(); //testBidder chooses Item from Auction 
+		assertEquals(testItem1, item);
 	}
+        
 	@Test
 	public void testchangeBid() {
-		testBidder.addBid(testAuction, testCredit);
-		testBidder.removeBid(testAuction, testCredit);
-		assertFalse(testBidder.viewBids().contains(testCredit));
+                //place first bid using $120 as the bid
+                testBidder.placeBid(testItem1);
+                Double bidAmount1 = testItem1.getBids().get("Chris");
+                
+                //place changed bid using $130 as the bid
+		testBidder.changeBid(testItem1);
+                Double bidAmount2 = testItem1.getBids().get("Chris");
+                
+                //see if the 2 bid amounts are not equals
+                assertFalse(bidAmount1 == bidAmount2);
 	}
 	@Test
 	public void testViewAuctionDetails() {
@@ -60,7 +101,7 @@ public class BidderTest {
 		System.out.println("Testing Empty Auction Calendar");
 		AuctionCalendar theCalendar = new AuctionCalendar();
 		assertTrue(theCalendar.getMyAuctions().isEmpty());
-		testAuction.viewCalendar(theCalendar);
+		//testAuction.viewCalendar(theCalendar); //dont need this
 		// The Test would be successful when the method exits with the print statement "The Auction List Is Empty"
 
 		// Test Non Empty Auction Calendar In the Auction Central
@@ -105,8 +146,18 @@ public class BidderTest {
 		theCalendar.addAuction(testAuction3);
 
 		// It should Print the Auction Details in the Auction Calendar
-		testAuction.viewAuctionDetails(theCalendar);
+		//testAuction.viewAuctionDetails(theCalendar); //viewAuctionDetails is not a method of Auction
+                testAuction.viewDetails();
 
 		System.out.println("Testing the viewAuctionDetails(AuctionCalendar theCalendar) method has Passed the Test Successfully");
 	}
+        
+        public static void main(String[] args) throws Exception {
+            BidderTest bt = new BidderTest();
+            bt.setUp();
+            bt.testBidder();
+            bt.testchooseItem();
+            bt.testchangeBid();
+            bt.testViewAuctionDetails();
+        }
 }
