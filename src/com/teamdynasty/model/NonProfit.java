592eb1b3766769a8139a773fb05d3dcaa0c4ff7f
shobagopi <shobagopi@gmail.com>
2015-12-05 21:08:21 +0530

    NonProfit Updated - Shoba

---
+++ src/com/teamdynasty/model/NonProfit.java
@@ -0,0 +1,74 @@
+package com.teamdynasty.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**NonProfit class which keeps track of the nonprofits auction and it's inventory.
+ * Created by Mat on 11/28/2015.
+ */
+public class NonProfit extends User {
+
+  private boolean isScheduled;
+  private String myOrgName;
+  private Auction myAuction;
+  private List<Item> myInventory;
+
+  public NonProfit(final String theUsername, final String theOrgName) {
+    super(theUsername, User.INPO);
+    myOrgName = theOrgName;
+    myAuction = null;
+    myInventory = new ArrayList<>();
+  }
+
+  public String getDashedName() {
+    return myOrgName.replace(" ", "-");
+  }
+
+  public Auction getMyAuction() {
+    return myAuction;
+  }
+
+//  public String getMyOrgName() {
+//    return myOrgName;
+//  }
+
+//  public void setMyOrgName(final String theOrgName) {
+//    myOrgName = theOrgName;
+//  }
+
+  public void setMyAuction(final Auction theAuction) {
+    myAuction = theAuction;
+  }
+
+  public boolean hasAuction() {
+    return (myAuction != null);
+  }
+
+  public boolean isScheduled() {
+    return isScheduled;
+  }
+
+  public Auction scheduleAuction() {
+    if (myAuction != null) {
+      isScheduled = true;
+    }
+    return myAuction;
+  }
+
+//  public void removeAuction() {
+//    myAuction = null;
+//  }
+
+  public List<Item> getMyInventory() {
+    return myInventory;
+  }
+
+  @Override
+  public String toString() {
+    return myOrgName;
+  }
+
+  public void addItemToInventory(Item theItem) {
+    myInventory.add(theItem);
+  }
+}
